313f1c868eb4f605f04074977f892ed38d78693d
Yuichi Takada <takadyuichi@gmail.com>
2017-04-02 00:48:18 +0900

    usres Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401145716) do
+ActiveRecord::Schema.define(version: 20170401151433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "kind",           null: false
+    t.string   "sentence",       null: false
+    t.string   "sound_file_url"
+    t.string   "correct_answer", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       null: false
