da91cec2f142f9722464d167a305900969e521dd
Yuichi Takada <takadyuichi@gmail.com>
2017-06-10 18:34:22 +0900

    Extract QuestionBoard class

--- client/app/bundles/components/Course.jsx
+++ client/app/bundles/components/Course.jsx
@@ -3,7 +3,7 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-import $ from 'jquery';
+import QuestionBoard from './QuestionBoard';
 
 const Course = ({ match }) => (
   <div>
@@ -20,120 +20,6 @@ const Course = ({ match }) => (
     <Route exact path={match.url} render={() => (
       <h3>Please select a course.</h3>
     )}/>
-  </div>
-);
-
-class QuestionBoard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {question: null, answerResult: null};
-    this.getQuestion = this.getQuestion.bind(this);
-    this.submitAnswer = this.submitAnswer.bind(this);
-    this.renderAnswerArea = this.renderAnswerArea.bind(this);
-
-    this.getQuestion();
-  }
-
-  getQuestion() {
-    $.ajax({
-      url: '/api/answers/new',
-      dataType: 'json',
-      success: function(question) {
-        this.setState({question: question, answerResult: null});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  }
-
-  submitAnswer(value) {
-    $.ajax({
-      url: '/api/answers',
-      dataType: 'json',
-      type: 'POST',
-      data: {
-        question_id: this.state.question.id,
-        value: value,
-        started_at: this.state.question.started_at,
-      },
-      success: function(answer) {
-        this.setState({answerResult: answer.result});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  }
-
-  renderAnswerArea() {
-    if (this.state.answerResult) {
-      return <AnswerResult result={this.state.answerResult} getNextQuestion={this.getQuestion}/>;
-    } else {
-      return <AnswerForm getNextQuestion={this.getQuestion} submitAnswer={this.submitAnswer} />;
-    }
-  }
-
-  render() {
-    if (this.state.question) {
-      return (
-        <div>
-          <h3>{this.props.match.params.courseId}</h3>
-          <Question question={this.state.question} />
-          {this.renderAnswerArea()}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h3>{this.props.match.params.courseId}</h3>
-        </div>
-      );
-    }
-  }
-}
-
-const Question = ({ question }) => (
-  <div>
-    <h4>{question.sentence}</h4>
-  </div>
-);
-
-class AnswerForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.submitAnswer(this.state.value);
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Answer:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-const AnswerResult = ({ result, getNextQuestion }) => (
-  <div>
-    <h3>{result}</h3>
-    <a href='javascript:void(0)' onClick={() => getNextQuestion()}>Next</a>
   </div>
 );
 
