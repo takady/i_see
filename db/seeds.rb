f82a3c85bde2b41287c0c7720c83af998b43aba5
Yuichi Takada <takadyuichi@gmail.com>
2017-06-10 23:20:16 +0900

    Make it possible to import source tsv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+ApplicationRecord.transaction do
+  Answer.delete_all
+  Question.delete_all
+
+  CSV.read(Rails.root.join('source.tsv'), col_sep: "\t").each.with_index(1) {|csv, index|
+    Question.create!(id: index, kind: :ja_to_en, sentence: csv.last, correct_answer: csv.first)
+  }
+end
